7313a0c7f7b050326f44ef42d57b9e1ab34acbd2
Baptiste Lubrano Lavadera <45080391+Mr-C4T@users.noreply.github.com>
2025-05-10 19:03:09 +0200

    Update LeCyborg-connect.sh

--- python-scripts/LeCyborg-connect.sh
+++ python-scripts/LeCyborg-connect.sh
@@ -2,4 +2,4 @@
 sudo rfcomm release all
 sudo rfcomm bind 0 78:21:84:8D:65:2A
 sudo chmod 777 /dev/rfcomm0
-python LeCyborg-client.py --mean 
+python LeCyborg-client.py --mean --mean-window 50 --threshold 600
