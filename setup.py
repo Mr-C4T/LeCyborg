931138eec9caf59bffc969a04fd5e722d4876569
Erwan <erwan.martin666@gmail.com>
2025-06-12 23:11:05 +0200

    class sensor + custom record working with esp

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LeCyborg',
+    version='0.1',
+    packages=find_packages(),
+    description='Lerobot Worldwide Hackathon 2025, using lerobot library with a bio muscle sensor',
+    author='Baptiste LUBRANO LAVADERA, Erwan MARTIN',
+    author_email='erwan.martin666@gmail.com',
+    install_requires=[
+    ],
+)
