f4fc2633b7638be6379d370dfd67dd098cd03235
Erwan <erwan.martin666@gmail.com>
2025-06-16 18:56:01 +0200

    re-add first-line comment

--- LeCyborg/custom_record.py
+++ LeCyborg/custom_record.py
@@ -1,3 +1,5 @@
+#THIS PROGRAM IS A MODIFIED VERSION OF THE record.py SCRIPT FROM LEROBOT@HUGGING FACE
+
 # Copyright 2024 The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
